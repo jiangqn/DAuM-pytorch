327636e656bcd49dbea1ff5693f3ab77f9bd08ac
ThomasK427 <1935448858@qq.com>
2018-12-29 20:08:12 +0800

    update'

---
+++ model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DAuM(nn.Module):
+
+    def __init__(self):
+        super(DAuM, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class Layer(nn.Module):
+
+    def __init__(self):
+        super(Layer, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class AdditiveAttention(nn.Module):
+
+    def __init__(self):
+        super(AdditiveAttention, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class MultiplicativeAttention(nn.Module):
+
+    def __init__(self):
+        super(MultiplicativeAttention, self).__init__()
+        pass
+
+    def forward(self):
+        pass

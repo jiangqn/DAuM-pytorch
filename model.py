2a44390d78e76d8e481652a9a43f78cce2d3e152
ThomasK427 <1935448858@qq.com>
2018-12-29 21:25:25 +0800

    update

--- model.py
+++ model.py
@@ -13,12 +13,25 @@ class DAuM(nn.Module):
 
 class Layer(nn.Module):
 
-    def __init__(self):
+    def __init__(self, model_size, lamda):
         super(Layer, self).__init__()
-        pass
+        self.aspect_attn = MultiplicativeAttention(model_size, model_size)
+        self.sentiment_sentiment_attn = AdditiveAttention(model_size, model_size)
+        self.sentiment_aspect_attn = AdditiveAttention(model_size, model_size)
+        self.lamda = lamda
 
-    def forward(self):
-        pass
+    def forward(self, sentiment, aspect, sentiment_memory, aspect_memory, mask):
+        aspect_attn = self.aspect_attn(aspect, aspect_memory).unsqueeze(1)
+        new_aspect = aspect_attn.matmul(aspect_memory).squeeze()
+        sentiment_sentiment_attn = self.sentiment_sentiment_attn(sentiment, sentiment_memory, mask).unsqueeze(1)
+        sentiment_aspect_attn = self.sentiment_aspect_attn(new_aspect, sentiment_memory, mask).unsqueeze(1)
+        sentiment_attn = (1 - self.lamda) * sentiment_sentiment_attn + self.lamda * sentiment_aspect_attn
+        sentiment = sentiment_attn.matmul(sentiment_memory)
+        aspect = aspect + new_aspect
+        return sentiment, aspect
+
+
+
 
 class AdditiveAttention(nn.Module):
 
@@ -26,16 +39,19 @@ class AdditiveAttention(nn.Module):
         super(AdditiveAttention, self).__init__()
         self.project = nn.Linear(query_size + key_size, 1)
 
-    def forward(self, query, key, mask):
+    def forward(self, query, key, mask=None):
         # query: (batch_size, query_size)
         # key: (batch_size, time_step, key_size)
         # mask: (batch_size, time_step)
         time_step = key.size(1)
         query = query.repeat(time_step, 1, 1).transpose(0, 1)   # (batch_size, time_step, query_size)
         scores = self.project(torch.cat([key, query], dim=2)).squeeze()     # (batch_size, time_step)
-        scores = scores - scores.max(dim=1, keepdim=True)[0]
-        scores = torch.exp(scores) * mask
-        attn_weights = scores / scores.sum(dim=1, keepdim=True)
+        if mask is not None:
+            scores = scores - scores.max(dim=1, keepdim=True)[0]
+            scores = torch.exp(scores) * mask
+            attn_weights = scores / scores.sum(dim=1, keepdim=True)
+        else:
+            attn_weights = F.softmax(scores, dim=1)
         return attn_weights
 
 class MultiplicativeAttention(nn.Module):
@@ -45,7 +61,7 @@ class MultiplicativeAttention(nn.Module):
         self.weights = nn.Parameter(torch.rand(key_size, query_size) * 0.2 - 0.1)
         self.bias = nn.Parameter(torch.zeros(1))
 
-    def forward(self, query, key, mask):
+    def forward(self, query, key, mask=None):
         # query: (batch_size, query_size)
         # key: (batch_size, time_step, key_size)
         # mask: (batch_size, time_step)
@@ -57,7 +73,10 @@ class MultiplicativeAttention(nn.Module):
         mids = mids.repeat(time_step, 1, 1, 1).transpose(0, 1)  # (batch_size, time_step, key_size, 1)
         key = key.unsqueeze(-2)  # (batch_size, time_step, 1, key_size)
         scores = torch.tanh(key.matmul(mids).squeeze() + self.bias).squeeze()  # (batch_size, time_step)
-        scores = scores - scores.max(dim=1, keepdim=True)[0]
-        scores = torch.exp(scores) * mask
-        attn_weights = scores / scores.sum(dim=1, keepdim=True)
+        if mask is not None:
+            scores = scores - scores.max(dim=1, keepdim=True)[0]
+            scores = torch.exp(scores) * mask
+            attn_weights = scores / scores.sum(dim=1, keepdim=True)
+        else:
+            attn_weights = F.softmax(scores, dim=1)
         return attn_weights
